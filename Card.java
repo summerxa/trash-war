3fe6be21c2b5794cf7d3091a8139e1b789d7be5d
Meenakshi Mukkamala <meenumukkamala@gmail.com>
2022-04-27 14:32:04 -0700

    test

--- Card.java
+++ Card.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
    The Card class represents a card in the game. It either has an image that
    represents a trash, recyclable, or compostable item, or has the word
@@ -16,22 +15,3 @@ public class Card
 
     
 }
-=======
-/**
-   The Card class represents a card in the game. It either has an image that
-   represents a trash, recyclable, or compostable item, or has the word
-   "Trash", "Recycle", or "Compost" written on it. The Card class will 
-   contain the mechanism to deal the cards randomly into the center  
-
-   @author  Meenakshi Mukkamala
-   @version 04/27/2022
-
-   @author  Sources - Anne, Vaishnavi
- */
-public class Card
-{
-
-
-    
-}
->>>>>>> df75e2b5adcd32a69c1785a6143aad121fb7b56b
