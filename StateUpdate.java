b786f27be9fccc97a09874078880b55c49de675f
x465xa <71350144+x465xa@users.noreply.github.com>
2022-05-03 14:55:23 -0700

    Create StateUpdate.java

---
+++ StateUpdate.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+/**
+ * Represents an update to the game state.
+ * 
+ * @author  Anne Xia
+ * @version 05/03/2022
+ * 
+ * @author Sources - Meenakshi, Vaishnavi
+ */
+public class StateUpdate {
+
+}
